4da2555dd75bcd30c1b1618b6cc449e5b5382845
resembleDeity <88549797+resembleDeity@users.noreply.github.com>
2024-12-13 19:36:07 +0800

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. Outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. Outputdir .. "/%{prj.name}")
+
+	files
+	{
+	    "imconfig.h",
+	    "imgui.h",
+	    "imgui.cpp",
+	    "imgui_draw.cpp",
+	    "imgui_internal.h",
+	    "imgui_widgets.cpp",
+	    "imstb_rectpack.h",
+	    "imstb_textdit.h",
+	    "imstb_truetype.h",
+	    "imgui_demo.cpp",
+	    "imgui_tables.cpp"                --For the newly "table" functions which introduced form ImGui v1.73
+	}
+
+	filter "system:windows"
+	    systemversion "latest"
+
+		defines
+    	{
+    	    "IMGUI_API=__declspec(dllexport)"
+    	}
+
+	filter "configurations:Debug"
+	    runtime "Debug"
+	    symbols "on"
+
+	filter "configurations:Release"
+	    runtime "Release"
+	    optimize "on"
