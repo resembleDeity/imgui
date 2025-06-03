d9d4bcd62247976dd5ae13129a79bda21fccebc7
TheCherno <y@nchernikov.com>
2025-06-03 17:51:14 +1000

    Added premake script

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+    staticruntime "off"
+    systemversion "latest"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp"
+	}
+
+	filter "system:linux"
+		pic "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
